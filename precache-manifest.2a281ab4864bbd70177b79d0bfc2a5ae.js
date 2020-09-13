821af6222f78e96a7141c34f2a148853555674a5
Deepak Jena <jenadeepak724@gmail.com>
2020-09-14 00:54:22 +0530

    Updates

---
+++ precache-manifest.2a281ab4864bbd70177b79d0bfc2a5ae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd29fc4360983cafd9cefb214573acd",
+    "url": "/iPod/index.html"
+  },
+  {
+    "revision": "e3914dd173e26cdcd8eb",
+    "url": "/iPod/static/css/main.bbf994ff.chunk.css"
+  },
+  {
+    "revision": "f1061e4156811701516f",
+    "url": "/iPod/static/js/2.dbb32e76.chunk.js"
+  },
+  {
+    "revision": "bf8487ee1bd81eaaff62cd5d9e511b97",
+    "url": "/iPod/static/js/2.dbb32e76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3914dd173e26cdcd8eb",
+    "url": "/iPod/static/js/main.96012d8f.chunk.js"
+  },
+  {
+    "revision": "97dba56fde2dd1bc0e68",
+    "url": "/iPod/static/js/runtime-main.3a9395a1.js"
+  },
+  {
+    "revision": "96f97a14a4ac57fdee46fe0387dcd6da",
+    "url": "/iPod/static/media/BadGuy.96f97a14.webm"
+  },
+  {
+    "revision": "26e431fd6bacf5db04b307f240581afb",
+    "url": "/iPod/static/media/COSTA RICA IN 4K 60fps HDR (ULTRA HD).26e431fd.mp4"
+  },
+  {
+    "revision": "231d481e14fbed3067a3c1c0dbf512ba",
+    "url": "/iPod/static/media/I_will_be_there_for_you.231d481e.webm"
+  },
+  {
+    "revision": "f9e1bda1f9d2b366a77484ee6ee11f01",
+    "url": "/iPod/static/media/Khairiyat.f9e1bda1.webm"
+  },
+  {
+    "revision": "d1f088c358c49bad7d548deaee69a7c8",
+    "url": "/iPod/static/media/Kun_Faya_Kun.d1f088c3.webm"
+  },
+  {
+    "revision": "1c94413d118f6d01a2be78be8c35c1b2",
+    "url": "/iPod/static/media/Laembadgini.1c94413d.webm"
+  }
+]);
